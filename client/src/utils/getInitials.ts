3c5b4b85092fbc2764ac1fef210b83ff89fb3af6
Burhaan-U-Deen Wani <burhaanwani823@gmail.com>
2026-08-27 16:46:00 +0530

    naming issue

--- client/src/utils/getInitials.ts
+++ client/src/utils/getInitials.ts
@@ -1,10 +1,3 @@
 export function getInitials(name: string): string {
-  const parts = name.split(" ");
-
-  const initials =
-    parts.length === 1
-      ? parts[0].slice(0, 2).toUpperCase()
-      : parts[0]?.[0].toUpperCase() + parts[1]?.[0].toUpperCase();
-
-  return initials;
+  return name.slice(0, 2);
 }
